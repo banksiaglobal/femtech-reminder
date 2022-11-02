a21152065ca758a0a7be1577c08677c0a7b3bb85
bg-dg <bg-dg@banksiaglobal.com>
2022-11-02 12:32:11 +0400

    fix

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StartPageModule } from './start-page/start-page/start-page.module';
 
 export const routes: Routes = [
   {
     path: 'overview',
     loadChildren: () => import('./start-page/start-page/start-page.module').then(m=>m.StartPageModule),
     pathMatch:'full'
-    
+
   },
   {
-    
       path: '',
-      redirectTo: '/overview',
+      redirectTo: 'overview',
       pathMatch: 'full',
-    
+  },
+  {
+    path: '**',
+    redirectTo: 'overview',
+    pathMatch: 'full',
   }
-
 ];
 
 @NgModule({
