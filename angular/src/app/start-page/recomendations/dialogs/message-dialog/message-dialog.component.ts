66e4009cab619c8e8e80f85d51d4843281aa5fd4
ShaustrukE <es@banksiaglobal.com>
2022-11-10 16:44:49 +0300

    add-remainders: add popup for successful request

--- angular/src/app/start-page/recomendations/dialogs/message-dialog/message-dialog.component.ts
+++ angular/src/app/start-page/recomendations/dialogs/message-dialog/message-dialog.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CreateRecomendationComponent } from '../create-recomendation/create-recomendation.component';
 
 @Component({
   selector: 'app-message-dialog',
   templateUrl: './message-dialog.component.html',
-  styleUrls: ['./message-dialog.component.scss']
+  styleUrls: ['./message-dialog.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatDialogModule, MatButtonModule]
+
 })
 export class MessageDialogComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public dialog: MatDialog,  public dialogRef: MatDialogRef<CreateRecomendationComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: any) {}
+    ngOnInit(): void {
   }
 
 }
